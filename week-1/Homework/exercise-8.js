7484044286068e31d4a3e31af01523ca661e2c5f
jorgesr92 <jorgesr92@github.com>
2020-09-04 10:07:19 +0200

    add part of ex 8

--- week-1/Homework/exercise-8.js
+++ week-1/Homework/exercise-8.js
@@ -101,3 +101,75 @@ var mentors = [
 
 //YOUR CODE HERE
 
+
+/* 1. Loop through the array, and for each object, `console.log()` out the sentence only for
+mentors that are in Barcelona and one of the skills is React
+"Hi, my name is {firstName} {lastName}. I work in Barcelona and i know React."
+*/
+mentors.filter(el => el.job.city === "Barcelona" && el.skills.includes("React")).forEach(el => console.log(`Hi, my name is ${el.firstName} ${el.lastName}. I work in Barcelona and i know React.`));
+/*
+2. To those that work in Barcelona, set "Jun1" in the class attribute, 
+and add a new skill to the list "SQL".
+To add elements in an array you can use .push()
+var animals = ["dog","cat"];
+animals.push("horse"); //["dog","cat","horse"]
+*/
+mentors.map(el =>{
+  if(el.job.city === "Barcelona") {
+    el.class = "Jun1";
+    el.skills.push("SQL");
+  }
+});
+
+console.log(mentors);
+/*
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+console.log(sequence);
+// → [1, 2, 3, 4, 5]
+
+3. Create an object method with the name .addSkill() to be able to add skills from it
+*/
+mentors.map(el => el.addSkill = function(newSkill){this.skills.push(newSkill)});
+mentors.map(el => el.addSkill("Angular"));
+console.log(mentors);
+/*
+4. Create a function to add a skill to all members in a list of mentors
+*/
+
+function addSkill(mentors,newSkill){
+  return mentors.map(el => el.skills.push(newSkill));
+  //your code here
+}
+
+addSkill(mentors, 'Angular');
+console.log(mentors);
+
+
+/*
+5. Create a function to remove a skill to all members in a list of mentors
+*/
+function removeSkill(mentors,newSkill){
+  return mentors.map(el => el.skills.splice(el.skills.indexOf(newSkill),1));
+  //your code here
+}
+removeSkill(mentors, 'Angular');
+console.log(mentors);
+/*
+6. Create a function mentorWithMoreSkills() that returns the name of the mentor with more number of skills
+*/
+
+function mentorWithMoreSkills(mentors){
+  return mentors.map(el => el.skills.length)
+}
+
+/*
+7. Create an object method .addStudentLikes() that increments by one the attribute studentLikes
+
+8. Create a function that adds a student like to all mentors in the array
+
+function addStudentLikes(mentors){
+  //your code here
+}*/
+
