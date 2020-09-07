7364e06476f29ebd615459d9f2e41ee86b27bf8f
jorgesr92 <jorgesr92@github.com>
2020-09-07 17:54:44 +0200

    add exercise 9

--- week-1/Homework/exercise-9.js
+++ week-1/Homework/exercise-9.js
@@ -41,9 +41,37 @@ var product2 = {
   price: 9.99,
   stock: 2
 };
+var product3 = {
+  id: 3,
+  name: "Iphone X",
+  price: 766.98,
+  stock: 80
+};
+var product4 = {
+  id: 4,
+  name: "Dell XPS",
+  price: 1876.99,
+  stock: 3
+};
+var product5 = {
+  id: 5,
+  name: "Funda Iphone",
+  price: 14.98,
+  stock: 150
+};
+var product6 = {
+  id: 6,
+  name: "Cargador Iphone",
+  price: 4.99,
+  stock: 6
+};
 
 products.push(product1);
 products.push(product2);
+products.push(product3);
+products.push(product4);
+products.push(product5);
+products.push(product6);
 
 var shoppingCart = {
   totalPrice: 0,
@@ -51,18 +79,37 @@ var shoppingCart = {
 };
 
 function addToShoppingCart(id){
-
-}
+  products.forEach(product => {
+    if (product.id === id){
+      shoppingCart.selectedProducts.push(product);
+      shoppingCart.totalPrice += product.price;
+    }
+  })
+};
 
 function removeFromShoppingCart(id){
-
+  shoppingCart.selectedProducts.forEach(product => {
+    if (product.id === id){
+      shoppingCart.selectedProducts.splice(shoppingCart.selectedProducts.indexOf(product),1);
+      shoppingCart.totalPrice -= product.price;
+    }
+  })
 }
 
 function shop(){
-
+  let ids = shoppingCart.selectedProducts.map(product => product.id);
+  products.forEach(product => {
+    if (ids.includes(product.id)){
+      product.stock -= 1;;
+    }
+  });
+  shoppingCart.totalPrice = 0;
+  shoppingCart.selectedProducts = [];
 }
 
 //results
+console.log("stocks: ");
+products.forEach(product => console.log(`${product.name}: ${product.stock}`));
 addToShoppingCart(1);
 console.log("Step 1");
 console.log("Total Price = " + shoppingCart.totalPrice);
@@ -88,3 +135,5 @@ console.log("Step 5");
 console.log("Total Price = " + shoppingCart.totalPrice);
 console.log("Number of Elements = " + shoppingCart.selectedProducts.length);
 console.log("Name of Elements = " + shoppingCart.selectedProducts.map(p=>p.name));
+console.log("stocks: ");
+products.forEach(product => console.log(`${product.name}: ${product.stock}`));
