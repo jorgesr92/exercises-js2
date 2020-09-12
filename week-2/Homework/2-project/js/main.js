dbd78312f50eafdb225a6cf18cef3a869ee9dbe8
jorgesr92 <jorgesr92@github.com>
2020-09-12 14:21:13 +0200

    class 2020.09.12

--- week-2/Homework/2-project/js/main.js
+++ week-2/Homework/2-project/js/main.js
@@ -3,96 +3,127 @@
 
 // part 1 homework
 
+//declaracion de variables
+
 let blueButton = document.querySelector('#blueBtn');
 let orangeButton = document.querySelector('#orangeBtn');
 let greenButton = document.querySelector('#greenBtn');
 
-let blueColors = [`#588fbd`, `#ffa500`, `black`];
-let orangeColors = [`#f0ad4e`, `#5751fd`, `#31b0d5`];
-let greenColors = [`#87ca8a`, `black`, `#8c9c08`];
+let paletaDeColores = {
+    blue:{
+        jumbotron : `#588fbd`,
+        donate : `#ffa500`,
+        volunteer : {
+            background : 'black',
+            text : 'white'
+        }
+    },
+    orange:{
+        jumbotron : `#f0ad4e`,
+        donate : `#5751fd`,
+        volunteer : {
+            background : '#31b0d5',
+            text : 'white'
+        }
+    },
+    green:{
+        jumbotron : `#87ca8a`,
+        donate : `black`,
+        volunteer : {
+            background : '#8c9c08',
+            text : 'white'
+        }
+    }
+}
+
+//funcion de cambio de color
 
 function changeBgdColor(colorOfButton){
-    let colors = [];
-    colorOfButton === 'blue'? colors = blueColors : colorOfButton === 'orange' ? colors = orangeColors : colors = greenColors;
     let divJumbotron = document.querySelector('.jumbotron');
-    divJumbotron.style.backgroundColor = colors[0];
-    let buttonOfDonate = document.querySelectorAll('.buttons a');
-    buttonOfDonate.forEach(button => buttonOfDonate[0] === button ? button.style.backgroundColor = colors[1]: button.style.backgroundColor = colors[2]);
+    divJumbotron.style.backgroundColor = paletaDeColores[colorOfButton].jumbotron;
+    let buttons = document.querySelectorAll('.buttons a');
+    buttons[0].style.backgroundColor = paletaDeColores[colorOfButton].donate;
+    buttons[1].style.backgroundColor = paletaDeColores[colorOfButton].volunteer.background;
+    buttons[1].style.color = paletaDeColores[colorOfButton].volunteer.text;
 }
 
-function clickOnBlue(){
-    changeBgdColor('blue');
-}
-function clickOnOrange(){
-    changeBgdColor('orange');
-}
-function clickOnGreen(){
-    changeBgdColor('green');
-}
+// interacción con los botones
 
-blueButton.addEventListener('click', clickOnBlue);
-orangeButton.addEventListener('click', clickOnOrange);
-greenButton.addEventListener('click', clickOnGreen);
+blueButton.addEventListener('click', ()=> changeBgdColor('blue'));
+orangeButton.addEventListener('click', ()=> changeBgdColor('orange'));
+greenButton.addEventListener('click', ()=> changeBgdColor('green'));
 
 
 // part 2 Homework
 
-let submitButton = document.querySelector('.row div form button');
-let formEmail = document.getElementById('exampleInputEmail1');
-let emailText;
-let formName = document.getElementById('example-text-input');
-let nameText;
-let formDescribe = document.getElementById('exampleTextarea');
-let describeText;
+//declaracion de variables
+
+const submitButton = document.querySelector('.row div form button');
+const formEmail = document.getElementById('exampleInputEmail1');
+const formName = document.getElementById('example-text-input');
+const formDescribe = document.getElementById('exampleTextarea');
 let errorsCounter = 0;
+
+// funcion para validar que hay texto en los campos del formulario
 
 function validationText(text){   
     return text.length > 0 ? true : false;
 }
 
+// validar la presencia de @ en el campo del email
+
 function validationAt(email){
-    console.log(`validation at: ${email}`);
-    return email.split('').includes('@') ? true : false;
+    return email.includes('@') ? true : false;
 }
+
+// restablecer los campos tras la validación positiva
 
 function borderOK(){
     formEmail.style.border = '1px solid rgba(0,0,0,.15)';
-    document.getElementById('exampleInputEmail1').value = "";
     formName.style.border = '1px solid rgba(0,0,0,.15)';
-    document.getElementById('example-text-input').value = "";
     formDescribe.style.border = '1px solid rgba(0,0,0,.15)';
-    document.getElementById('exampleTextarea').value = "";
+    document.forms[0].reset();
 }
 
+// función para poner en marcha todas las validaciones
+
 function checkAll(){
-    let atPresent = validationAt(emailText) ? true : false;
-    let isEmailTextPresent = validationText(emailText) ? true : false;
+    let atPresent = validationAt(formEmail.value) ? true : false;
+    let isEmailTextPresent = validationText(formEmail.value) ? true : false;
     atPresent && isEmailTextPresent ? console.log("Email OK") : formEmail.style.border = '1px solid red';
-    let namePresent = validationText(nameText) ? true : false;
+    let namePresent = validationText(formName.value) ? true : false;
     namePresent ? console.log("Name OK") : formName.style.border = '1px solid red';
-    let describePresent = validationText(describeText) ? true : false;
+    let describePresent = validationText(formDescribe.value) ? true : false;
     describePresent ? console.log("Describe OK") : formDescribe.style.border = '1px solid red'
     let isAllOK = atPresent && isEmailTextPresent && namePresent && describePresent ? true : false;
-    isAllOK ? borderOK() : console.log('Hay Errores!');
+    isAllOK ? borderOK() : console.warn('Hay Errores!');
     return isAllOK;
 }
 
+// función para ejecutar checkAll y mostrar alert de gracias
+
 function alertOk(){
-    emailText = formEmail.value;
-    nameText = formName.value;
-    describeText = formDescribe.value;
+    let name = formName.value;
     let allOK = checkAll() ? true: false;
-    allOK ? alert(`Muchas gracias ${nameText} por registrate!!`) : errorsCounter ++;
-    errorsCounter === 3 ? seriousAlert() : console.log(`Error, ¡${errorsCounter}º aviso!`);
-    return allOK
+    allOK ? alert(`Muchas gracias ${name} por registrate!!`) : errorsCounter ++;
+    if (!allOK && errorsCounter < 3) {
+        console.warn(`Error, ¡${errorsCounter}º aviso!`);
+    } else if (!allOK && errorsCounter === 3) {
+        seriousAlert() 
+    }
+    return allOK;
 }
 
+// alert en caso de equivocarte 3 veces seguidas
+
 function seriousAlert(){
-    alert(`Error, ¡${errorsCounter}º aviso! \nPara añadir un párrafo ES NECESARIO QUE INTRODUZCAS TEXTO EN EL FORMULARIO! \nGracias.`);
+    confirm(`Error, ¡${errorsCounter}º aviso! \nPara añadir un párrafo ES NECESARIO QUE INTRODUZCAS TEXTO EN EL FORMULARIO! \nGracias.`);
     errorsCounter = 0;
 }
 
-submitButton.addEventListener('click', function(event){
+// interacción con el botón submit
+
+submitButton.addEventListener('click', (event) => {
     event.preventDefault();
     alertOk();
-},true);
+});
