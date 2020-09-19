f7bf5931d7d9b4e9fda58dde65f8ac2173a94181
jorgesr92 <jorgesr92@github.com>
2020-09-19 14:34:16 +0200

    update exercise 2

--- week-3/Homework/2-slideshow/slideshow.js
+++ week-3/Homework/2-slideshow/slideshow.js
@@ -1,21 +1,24 @@
-// Write your code here
-let slides = {
-    firstSlide:{
+
+let slides = [
+    {
         id:0,
         url:'https://cyf-image-carousel.netlify.app/sample-images/kitten2.jpg'
     },
-    secondSlide: {
+    {
         id:1,
         url:'https://cyf-image-carousel.netlify.app/sample-images/kitten3.jpg'
     },
-    thirdSlide: {
+    {
         id:2,
         url:'https://cyf-image-carousel.netlify.app/sample-images/kitten1.jpg'
     }
-};
+];
 
 let slideNow = 0;
 let stopInterval = -1;
+
+document.getElementById('divCenter').style.textAlign = "center";
+document.getElementById('center').style.textAlign = "center";
 
 const buttonAutoBack = document.getElementById('autoBack');
 const buttonBack = document.getElementById("back");
@@ -26,24 +29,58 @@ const buttonAutoForward = document.getElementById('autoForward');
 const imgCarousel = document.getElementById('images');
 
 function nextImage(){
-    slideNow < slides.length ? slideNow++ : slideNow = 0;
-    imgCarousel.src = slides.forEach(slide => {
+    slideNow < slides.length-1 ? slideNow++ : slideNow = 0;
+    console.log(slideNow);
+    slides.forEach(slide => {
         if (slide.id === slideNow){
-            slide.url;
+            imgCarousel.src = slide.url;
         }
     });
-}
+};
 
 function previusImage(){
-    slideNow < slides.length ? slideNow-- : slideNow = 0;
-    imgCarousel.src = slides.forEach(slide => {
+    slideNow != 0 ? slideNow-- : slideNow = slides.length-1;
+    console.log(slideNow);
+    slides.forEach(slide => {
         if (slide.id === slideNow){
-            slide.url;
+            imgCarousel.src = slide.url;
         }
     });
+};
+
+function timerCarousel(){
+    let timer = document.getElementById('time').value;
+    timer <= 0 ? timer = 1000 : timer = timer*1000;
+    return timer;
 }
 
-buttonBack.addEventListener("click", ()=>{
-    console.log('Hola Mundo');
+buttonBack.addEventListener("click", (_event)=>{
+    clearInterval(stopInterval);
+    previusImage();
+});
+
+buttonForward.addEventListener("click", (_event)=>{
+    clearInterval(stopInterval);
+    nextImage();
+});
+
+buttonAutoBack.addEventListener("click", (_event) =>{
+    clearInterval(stopInterval);
+    let timerCa = timerCarousel();
+    stopInterval = setInterval(() => {
+        previusImage();
+    },timerCa);
+});
+
+buttonAutoForward.addEventListener("click", (_event) =>{
+    clearInterval(stopInterval);
+    let timerCa = timerCarousel();
+    stopInterval = setInterval(() => {
+        nextImage();
+    },timerCa);
+});
+
+buttonStop.addEventListener("click", (_event) => {
+    clearInterval(stopInterval);
 })
 
